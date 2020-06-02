7f3146a3679f8d9fde3058d2e0915c57d87024ef
mxmntv <mxmmmntv@gmail.com>
2020-06-02 22:24:46 +0300

    test pong

--- stringutil.go
+++ stringutil.go
@@ -1,7 +1,7 @@
 package stringutil
 
 // Reverse возвращает его строку аргумент
-// развернутую в обратном порядке.
+// развернутую в обратном порядке!.
 func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
